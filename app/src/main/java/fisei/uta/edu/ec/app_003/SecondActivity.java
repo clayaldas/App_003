67414d743c7c2a25f99d31a3c1fa2d5738df91cb
Clay Aldas <clayaldas@gmail.com>
2022-11-17 20:09:52 -0500

    creado el tercer activity el el ListView

--- app/src/main/java/fisei/uta/edu/ec/app_003/SecondActivity.java
+++ app/src/main/java/fisei/uta/edu/ec/app_003/SecondActivity.java
@@ -2,12 +2,20 @@ package fisei.uta.edu.ec.app_003;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
     private TextView textViewTitle;
+    private Button buttonShowBrowser;
+    private Button buttonCallPhoneNumber;
+    private Button buttonShowActivity;
+    private Button buttonFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +31,26 @@ public class SecondActivity extends AppCompatActivity {
         textViewTitle = findViewById(R.id.textViewTitle);
 
         textViewTitle.setText("Usuario:" + userName + "     " + "Clave: " + password);
+    }
+
+    public void onClick_buttonShowBrowser(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+        startActivity(intent);
+    }
+
+    public void onClick_buttonCallPhoneNumber (View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: +09930201510"));;
+        startActivity(intent);
+    }
+
+    public void onClick_buttonShowActivity (View view) {
+        //Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onClick_buttonFinish (View view) {
+        finish();
     }
 }
---
+++ app/src/main/java/fisei/uta/edu/ec/app_003/ThirdActivity.java
@@ -0,0 +1,14 @@
+package fisei.uta.edu.ec.app_003;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
