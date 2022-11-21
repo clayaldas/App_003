0c38d73c667ad4f34f6bc7ee07c759df327f5331
Clay Aldas <clayaldas@gmail.com>
2022-11-20 22:55:24 -0500

    Agregado regreso de parametros, menus y menus contextuales

--- app/src/main/java/fisei/uta/edu/ec/app_003/MainActivity.java
+++ app/src/main/java/fisei/uta/edu/ec/app_003/MainActivity.java
@@ -1,9 +1,13 @@
 package fisei.uta.edu.ec.app_003;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -51,6 +55,82 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // menu contextual
+        // asociar el menu contextual con un View (control: editTextPassword)
+        registerForContextMenu(editTextPassword);
+
+        getSupportActionBar().setTitle("Usuario conectado" + R.string.app_name);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch ( item.getItemId()) {
+            case R.id.menu_insert:
+                Toast.makeText(this, "Presiono en: Insertar", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.menu_delete:
+                Toast.makeText(this, "Presiono en: Eliminar", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.menu_update:
+                Toast.makeText(this, "Presiono en: Modificar", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.menu_search:
+                Toast.makeText(this, "Presiono en: buscar", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.menu_open:
+                Intent intent = new Intent(this, FourthActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.menu_edit_copy:
+                Toast.makeText(this, "Presiono en: copiar", Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+        //return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        menu.setHeaderTitle("Menu Contextual");
+
+        // mostrar el menu contextual en el activity actual
+        getMenuInflater().inflate(R.menu.menu_contextual, menu);
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_contextual_cut:
+                Toast.makeText(this, "Presiono en: cortar", Toast.LENGTH_LONG).show();
+               break;
+
+            case R.id.menu_contextual_copy:
+                Toast.makeText(this, "Presiono en: copiar", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.menu_contextual_paste:
+                Toast.makeText(this, "Presiono en: pegar", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return super.onContextItemSelected(item);
+    }
 }
